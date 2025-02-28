09579276394c82602c4c9fea9ef857e38edff1a3
Axuata <axuata@gmail.com>
2025-02-28 11:01:48 +0900

    Updated Packages

--- components/TestComponent.vue
+++ components/TestComponent.vue
@@ -5,12 +5,10 @@
 <template>
   <div>
     <div class="i-tabler:template"></div>
-    <p class="font-roboto text-100px">This is a component.</p>
+    <p class="text-100px">This is a component.</p>
   </div>
 </template>
 
 <style>
-.font-roboto {
-  font-family: Roboto, sans-serif;
-}
+
 </style>
